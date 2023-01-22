648c07617b5e9f80afdecab4d36a2c815e30e7c1
seungjun-green <lsj3285007@gmail.com>
2023-01-22 18:09:51 +0900

    Initial Commit

---
+++ DualNBack/DualNBackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualNBackApp.swift
+//  DualNBack
+//
+//  Created by SeungJun Lee on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DualNBackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
